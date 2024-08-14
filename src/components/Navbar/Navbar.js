c256c2473ea49f930e9c9e48908e80f5369a4412
Angel Yair Vega Reyes <yairvreyes@pricecorp.priceshoes.com.mx>
2024-08-14 01:55:49 -0600

    Haciendo la Navbar responsive

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,28 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.css'; // Asegúrate de que el archivo CSS esté en la misma carpeta
-import logo from '../../images/LOGO.jpg';
+import './Navbar.css';
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <Link to="/" className="navbar-brand"> {/* Cambia a Link */}
-          <img src={logo} alt="GOVINDA OM Logo" className="navbar-logo" />
+        <Link to="/" className="navbar-brand">
+          <img src="../images/LOGO.jpg" alt="GOVINDA OM Logo" className="navbar-logo" />
           GOVINDA OM
         </Link>
-        <ul className="navbar-menu">
+        <button className="navbar-toggler" onClick={toggleMenu}>
+          ☰
+        </button>
+        <ul className={`navbar-menu ${isMenuOpen ? 'active' : ''}`}>
           <li className="navbar-item">
-            <Link to="/" className="navbar-link">Inicio</Link> {/* Cambia a Link */}
+            <Link to="/" className="navbar-link">Inicio</Link>
           </li>
           <li className="navbar-item">
-            <Link to="/about" className="navbar-link">Conócenos</Link> {/* Cambia a Link */}
+            <Link to="/about" className="navbar-link">Conócenos</Link>
           </li>
           <li className="navbar-item">
-            <Link to="/courses" className="navbar-link">Cursos</Link> {/* Cambia a Link */}
+            <Link to="/courses" className="navbar-link">Cursos</Link>
           </li>
           <li className="navbar-item">
-            <Link to="/store" className="navbar-link">Tienda</Link> {/* Cambia a Link */}
+            <Link to="/store" className="navbar-link">Tienda</Link>
           </li>
         </ul>
       </div>
